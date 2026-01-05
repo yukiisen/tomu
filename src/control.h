f5d8789839d8068e9abbc11ea4a1fcef4f66b676
pixel2175 <pixel2176@proton.me>
2026-01-05 15:52:22 +0100

    feat: add path_handle and improve shuffle logic

--- src/control.h
+++ src/control.h
@@ -8,6 +8,7 @@ void *control_place(void *arg);
 void playback_pause(PlayBackState *state);
 void playback_resume(PlayBackState *state);
 void playback_stop(PlayBackState *state);
+void path_handle(const char *path);
 void shuffle(const char *path);
 
 #endif
