5bd13629136e057392a4efc595888eb4feaa4dc4
6z7y <eenanho24n@proton.me>
2026-01-10 10:50:16 +0300

    fixing the argument parsing first

--- src/main.c
+++ src/main.c
@@ -2,35 +2,43 @@
 #include <string.h>
 #include <libgen.h>
 
-// #include "backend.h"
 #include "control.h"
-// #include "utils.h"
+#include "utils.h"
 
-#define VERSION "0.0.6"
+#define PROG_NAME "tomu"
+#define PROG_VER "0.0.7"
 
 int main(int argc, char *argv[]){
   if (argc < 2 ){
-    printf("Usage: %s [FILE]\n", basename((argv[0])));
+    printf("Usage: %s [FILE]\n", PROG_NAME);
     return 0;
   }
 
   const char *command = argv[1];
   const char *filename = argv[argc - 1];
 
-  if (strcmp("--loop", command) == 0 )
-    while (1) path_handle(filename);
+  // first check about an arg options "--"
+  if ( (argv[1][0] == '-') && (argv[1][1] == '-') )
 
-  else if (strcmp("--shuffle-loop", command) == 0 )
-    while (1) shuffle(filename);
+    if (strcmp("--loop", command) == 0 )
+      while (1) path_handle(filename);
 
-  else if (strcmp("--help", command) == 0 )
-    help();
+    else if (strcmp("--shuffle-loop", command) == 0 )
+      while (1) shuffle(filename);
 
-  else if (strcmp("--version", command) == 0 )
-    printf("%s\n", VERSION);
+    else if (strcmp("--help", command) == 0 )
+      help();
 
-  else
-	  path_handle(filename);
+    else if (strcmp("--version", command) == 0 )
+      printf("%s\n", PROG_VER);
+
+    else {
+      printf("[T] unknown command '%s'\n", command);
+      return 1;
+    }
+
+  else 
+    path_handle(filename);
 
   return 0;
 }
