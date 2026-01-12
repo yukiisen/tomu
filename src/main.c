24891131900101c547003f1ed9a3d38c3b7183a1
6z7y <eenanho24n@proton.me>
2026-01-13 00:52:05 +0300

    not much changed

--- src/main.c
+++ src/main.c
@@ -1,44 +1,40 @@
 #include <stdio.h>
 #include <string.h>
-#include <libgen.h>
 
 #include "control.h"
 #include "utils.h"
 
 #define PROG_NAME "tomu"
-#define PROG_VER "0.0.7"
+#define PROG_VER "0.0.8"
 
-int main(int argc, char *argv[]){
-  if (argc < 2 ){
-    printf("Usage: %s [FILE]\n", PROG_NAME);
+int main(int argc, char *argv[])
+{
+  if (argc < 2){
+    printf("Usage: %s [File.mp3]\n", PROG_NAME);
     return 0;
   }
 
-  const char *command = argv[1];
+  const char *arg = argv[1];
   const char *filename = argv[argc - 1];
 
-  // first check about an arg options "--"
-  if ( (argv[1][0] == '-') && (argv[1][1] == '-') )
+  if (argv[1][0] == '-' && argv[1][1] == '-') {
 
-    if (strcmp("--loop", command) == 0 )
-      while (1) path_handle(filename);
+    if (strcmp("--loop", arg) == 0)
+      while (1) handle_input(&filename);
 
-    else if (strcmp("--shuffle-loop", command) == 0 )
+    else if (strcmp("--shuffle-loop", arg) == 0)
       while (1) shuffle(filename);
 
-    else if (strcmp("--help", command) == 0 )
+    else if (strcmp("--help", arg) == 0)
       help();
 
-    else if (strcmp("--version", command) == 0 )
+    else if (strcmp("--version", arg) == 0)
       printf("%s\n", PROG_VER);
 
-    else {
-      printf("[T] unknown command '%s'\n", command);
-      return 1;
-    }
+    else 
+      printf("[T] Unknown Arg '%s'\n", arg);
+  }
 
-  else 
-    path_handle(filename);
-
+  path_handle(filename);
   return 0;
 }
